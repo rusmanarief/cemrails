cced3d53809274d4ea9bec7f8975bb117c08c721
Rusman Arief <rusmanarief@gmail.com>
2014-01-15 22:11:52 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cemrails::Application.config.secret_token = '67c0c37a58c0cd8bc8258823e4e50777d2d2786e3de9179b2281edee719c8b54c47be6ffe43ff603a04ac98a66fa4e65dd40ef34d9294896400c49001c6a2616'
